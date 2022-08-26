30072442e90a271964b09ba76350a683ff1af7a7
Kevin Rangel Miguel <56046361+volpix@users.noreply.github.com>
2022-08-26 12:15:50 -0500

    Consumo de Web Service con Paginado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebService_ACFTECHNOLOGIES.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ wwwroot/js/Lista.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () { //click
+    GetAll();
+});
+
+function GetAll() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://jsonplaceholder.typicode.com/photos',
+        success: function (result) { //200 OK    
+            $('#tbl tbody').empty();
+            $.each(result, function (index, item) {
+                var fila =
+                    '<tr>'
+                    + "<td class='text-center'>" + item.title + "</td>"
+                    + "<td class='text-center'><img src=" + item.thumbnailUrl + "></td>"
+                    + "</tr>";
+                $("#tbl tbody").append(fila);
+            });
+        },
+        error: function (result) {
+            alert('Error en la consulta.' + result.responseJSON.ErrorMessage);
+        }
+    });
+};
